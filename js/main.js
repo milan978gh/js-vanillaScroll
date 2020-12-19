33610dca844ae11ebae3010b421590e2553673b3
milan978gh <amilandev78@gmail.com>
2020-12-19 20:39:34 +0100

    js done

--- js/main.js
+++ js/main.js
@@ -1,16 +1,23 @@
-window.addEventListener('scroll', (e) => {
+window.addEventListener('scroll', () => {
   const target = document.querySelectorAll('.scroll');
   // const scrolled = window.pageYOffset;
   // const rate = scrolled * -2;
-  let index = 0;
-  const length = target.length;
 
   // target.style.transform = `translate3d(0px, ${rate}px, 0px)`;
 
+  let index = 0;
+  const length = target.length;
+
+
   for (index; index < length; index++) {
     const pos = window.pageYOffset * target[index].dataset.rate;
-    target[index].style.transform = `translate3d(0px, ${pos}px, 0px)`;
+    const posX = window.pageYOffset * target[index].dataset.ratex;
+    const posY = window.pageYOffset * target[index].dataset.ratey;
+
+    if (target[index].dataset.direction === 'vertical') {
+      target[index].style.transform = `translate3d(0px, ${pos}px, 0px)`;
+    } else {
+      target[index].style.transform = `translate3d(${posX}px, ${posY}px, 0px)`;
+    }
   }
-
-
 });
