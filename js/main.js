0e8c20700b4dc7776798296b045ba89ab944efa0
milan978gh <amilandev78@gmail.com>
2020-12-19 19:52:22 +0100

    html update

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
 window.addEventListener('scroll', (e) => {
   const target = document.querySelectorAll('.scroll');
-})
+  // const scrolled = window.pageYOffset;
+  // const rate = scrolled * -2;
+  let index = 0;
+  const length = target.length;
+
+  // target.style.transform = `translate3d(0px, ${rate}px, 0px)`;
+
+  for (index; index < length; index++) {
+    const pos = window.pageYOffset * target[index].dataset.rate;
+    target[index].style.transform = `translate3d(0px, ${pos}px, 0px)`;
+  }
+
+
+});
